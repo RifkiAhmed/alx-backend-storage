eab881d754f51949fd87b03b58b7ba2b0f890c85
ahmed <rifkiahmed.91@gmail.com>
2024-01-23 00:56:06 +0100

    Change attribute vale of a document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''Changes all topics of a school document based on the name'''
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a school document based on the name'''
+    mongo_collection.update_many({"name": name},
+                                 {"$set": {"topics": topics}})
+
+
+# list_all = __import__('8-all').list_all
+# if __name__ == "__main__":
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     school_collection = client.my_db.school
+#     update_topics(school_collection,
+#                   "Holberton school",
+#                   ["Sys admin", "AI", "Algorithm"])
+
+#     schools = list_all(school_collection)
+#     for school in schools:
+#         print("[{}] {} {}".format(school.get('_id'),
+#                                   school.get('name'),
+#                                   school.get('topics', "")))
+
+#     update_topics(school_collection, "Holberton school", ["iOS"])
+
+#     schools = list_all(school_collection)
+#     for school in schools:
+#         print("[{}] {} {}".format(school.get('_id'),
+#                                   school.get('name'),
+#                                   school.get('topics', "")))
