37a25e6f87f518418c0a6eb6989169181cf96956
ahmed <rifkiahmed.91@gmail.com>
2024-01-23 21:02:46 +0100

    Add tabulation

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     client = MongoClient("localhost", 27017)
     db = client["logs"]
     collection = db["nginx"]
-    print(f"{collection.count_documents({})} logs")
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print("    method {}: {}".format(
+        print("\tmethod {}: {}".format(
             method,
             collection.count_documents({"method": method})
         ))
