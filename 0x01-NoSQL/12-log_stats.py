39fb11c7979e901dfbaf0bbd6799ac8659f896b3
ahmed <rifkiahmed.91@gmail.com>
2024-01-23 20:59:30 +0100

    Make script runs as the main program only

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 '''Provide some stats about Nginx logs stored in mongoDB'''
-from pymongo import MongoClient
 
-client = MongoClient("localhost", 27017)
-db = client["logs"]
-collection = db["nginx"]
-print(f"{collection.count_documents({})} logs")
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    print("    method {}: {}".format(
-        method,
-        collection.count_documents({"method": method})
+if __name__ == "__main__":
+    from pymongo import MongoClient
+
+    client = MongoClient("localhost", 27017)
+    db = client["logs"]
+    collection = db["nginx"]
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("    method {}: {}".format(
+            method,
+            collection.count_documents({"method": method})
+        ))
+
+    print("{} status check".format(
+        collection.count_documents({'method': 'GET', 'path': '/status'})
     ))
-
-print("{} status check".format(
-    collection.count_documents({'method': 'GET', 'path': '/status'})
-))
