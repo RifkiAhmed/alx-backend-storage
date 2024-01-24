c956737da4af2ede00ff63ebab56034bbc497f72
ahmed <rifkiahmed.91@gmail.com>
2024-01-25 00:58:33 +0100

    Define a decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 '''Cache class'''
+from functools import wraps
 import redis
 from typing import Union, Optional, Callable
+import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Count how many times methods are called'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        ''''''
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,9 +25,9 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Store the input data in Redis instance'''
-        import uuid
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -34,14 +47,11 @@ class Cache:
         return self.get(key, int)
 
 
-cache = Cache()
+# cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
+# cache.store(b"first")
+# print(cache.get(cache.store.__qualname__))
 
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+# cache.store(b"second")
+# cache.store(b"third")
+# print(cache.get(cache.store.__qualname__))
