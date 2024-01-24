b6d9c06604016900ef5549bf35486900df81f7c3
ahmed <rifkiahmed.91@gmail.com>
2024-01-25 00:00:42 +0100

    Add get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Cache class'''
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -19,12 +19,28 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable]
+            ) -> Union[None, str, bytes, int, float]:
+        '''Return the key value converted to desired format'''
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        '''Return the key value converted to string format'''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        '''Return the key value converted to integer format'''
+        return self.get(key, int)
+
 
 # cache = Cache()
 
-# data = b"hello"
-# key = cache.store(data)
-# print(key)
+# TEST_CASES = {
+#     b"foo": None,
+#     123: int,
+#     "bar": lambda d: d.decode("utf-8")
+# }
 
-# local_redis = redis.Redis()
-# print(local_redis.get(key))
+# for value, fn in TEST_CASES.items():
+#     key = cache.store(value)
+#     assert cache.get(key, fn=fn) == value
