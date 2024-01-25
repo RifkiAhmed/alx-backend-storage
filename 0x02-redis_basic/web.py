a375c51826928b95602cf5d48859dc719f0dc0ea
ahmed <rifkiahmed.91@gmail.com>
2024-01-25 03:56:28 +0100

    Decorator that tracks and sets an expired web cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''Implementing an expiring web cache and tracker'''
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+
+def cache_with_expiry(method: Callable) -> Callable:
+    '''Define a decorator that tracks and sets an expired web cache'''
+    @wraps(method)
+    def wrapper(*args):
+        _redis = redis.Redis()
+        _redis.incr("count:{}".format(args[0]))
+        cached = _redis.get("{}".format(args[0]))
+        if cached:
+            return cached.decode("utf-8")
+        response = method(args[0])
+        _redis.set("{}".format(args[0]), response, 10)
+        return response
+    return wrapper
+
+
+@cache_with_expiry
+def get_page(url: str) -> str:
+    response = requests.get(url)
+    return response.text
